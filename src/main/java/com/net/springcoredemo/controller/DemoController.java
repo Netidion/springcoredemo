45b3c5c99dbc6206dddea52b8c792ab907d7d765
Neti <98841356+Netidion@users.noreply.github.com>
2023-12-05 17:14:26 +0200

    Lazy annotation usage

--- src/main/java/com/net/springcoredemo/controller/DemoController.java
+++ src/main/java/com/net/springcoredemo/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.net.springcoredemo.controller;
 
 import com.net.util.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,8 @@ public class DemoController {
      * Qualifier has higher priority than Primary
      */
     @Autowired
-    public DemoController(Coach theCoach){
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+        System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
 
--- src/main/java/com/net/springcoredemo/service/TrackCoach.java
+++ src/main/java/com/net/springcoredemo/service/TrackCoach.java
@@ -1,12 +1,17 @@
 package com.net.springcoredemo.service;
 
 import com.net.util.Coach;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@Lazy
 public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Run a hard 5k!";
